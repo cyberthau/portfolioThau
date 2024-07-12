e187382a2fc958ce852cad1b49359c87338b7806
cristhau <thaucris@gmail.com>
2024-07-11 22:01:29 -0300

    portfolio  thauanna

---
+++ script.js
@@ -0,0 +1,8 @@
+// Scrip validação do formulário
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulário enviado com sucesso!');
+
+});
+
